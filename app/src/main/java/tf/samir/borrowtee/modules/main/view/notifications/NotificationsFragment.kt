03eb1c4ba8b1dd426d6818aa70ddfa027337d913
samirtf <samircc20092@gmail.com>
2020-10-04 00:02:45 -0300

    refactor: change databinding and viewBinding config; replace deprecated calls on view model; add thing at home item

--- app/src/main/java/tf/samir/borrowtee/modules/main/view/notifications/NotificationsFragment.kt
+++ app/src/main/java/tf/samir/borrowtee/modules/main/view/notifications/NotificationsFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import tf.samir.borrowtee.R
 import tf.samir.borrowtee.modules.main.presentation.presenter.notifications.NotificationsViewModel
@@ -21,7 +22,7 @@ class NotificationsFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         notificationsViewModel =
-                ViewModelProviders.of(this).get(NotificationsViewModel::class.java)
+                ViewModelProvider(this).get(NotificationsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_notifications, container, false)
         val textView: TextView = root.findViewById(R.id.text_notifications)
         notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
