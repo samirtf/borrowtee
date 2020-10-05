004ba00b5df1f1ca81f9372b23400faa36609294
samirtf <samircc20092@gmail.com>
2020-10-05 19:55:25 -0300

    add things attribute to AllThingsViewModel

--- app/src/main/java/tf/samir/borrowtee/modules/main/presentation/presenter/all_things/AllThingsViewModel.kt
+++ app/src/main/java/tf/samir/borrowtee/modules/main/presentation/presenter/all_things/AllThingsViewModel.kt
@@ -3,11 +3,21 @@ package tf.samir.borrowtee.modules.main.presentation.presenter.all_things
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import tf.samir.borrowtee.modules.main.domain.entities.Thing
 
 class AllThingsViewModel : ViewModel() {
 
+    private val things = MutableLiveData<Thing>()
     private val _text = MutableLiveData<String>().apply {
         value = "This is All Things Fragment"
     }
     val text: LiveData<String> = _text
+
+    init {
+        loadData()
+    }
+
+    private fun loadData() {
+        throw NotImplementedError("fetch data from repository")
+    }
 }
