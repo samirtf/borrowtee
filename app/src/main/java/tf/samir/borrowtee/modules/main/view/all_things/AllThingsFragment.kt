579eabf81c2cd21a27b333415a71164473ee2ed0
samirtf <samircc20092@gmail.com>
2020-10-05 22:31:57 -0300

    feature: remove unused imports

--- app/src/main/java/tf/samir/borrowtee/modules/main/view/all_things/AllThingsFragment.kt
+++ app/src/main/java/tf/samir/borrowtee/modules/main/view/all_things/AllThingsFragment.kt
@@ -4,12 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import tf.samir.borrowtee.R
 import tf.samir.borrowtee.databinding.FragmentAllThingsBinding
 import tf.samir.borrowtee.modules.main.presentation.presenter.all_things.AllThingsViewModel
 
--- app/src/main/java/tf/samir/borrowtee/modules/main/view/notifications/NotificationsFragment.kt
+++ app/src/main/java/tf/samir/borrowtee/modules/main/view/notifications/NotificationsFragment.kt
@@ -8,7 +8,6 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import tf.samir.borrowtee.R
 import tf.samir.borrowtee.modules.main.presentation.presenter.notifications.NotificationsViewModel
 
