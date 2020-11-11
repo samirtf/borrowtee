7543eb3362c26824e1ad37c84599756146d46d2b
samirtf <samircc20092@gmail.com>
2020-11-11 00:54:07 -0300

    refactoring: use distinct until changed experimental feature

--- app/src/main/java/tf/samir/borrowtee/modules/main/infrastructure/dao/ThingModelDao.kt
+++ app/src/main/java/tf/samir/borrowtee/modules/main/infrastructure/dao/ThingModelDao.kt
@@ -1,7 +1,9 @@
 package tf.samir.borrowtee.modules.main.infrastructure.dao
 
 import androidx.room.*
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.distinctUntilChanged
 import tf.samir.borrowtee.modules.main.infrastructure.model.ThingModel
 import tf.samir.borrowtee.modules.main.infrastructure.model.tableNameThing
 
@@ -10,6 +12,9 @@ interface ThingModelDao {
 
     @Query("SELECT * FROM $tableNameThing ORDER BY name ASC")
     fun getAllThings(): Flow<List<ThingModel>>
+
+    @ExperimentalCoroutinesApi
+    fun getAllThingsDistinctUntilChanged() = getAllThings().distinctUntilChanged()
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(thing: ThingModel)
