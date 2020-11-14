0347433bc3c1e041ac5bf82fef988eb3cdcd8dfe
samirtf <samircc20092@gmail.com>
2020-11-14 00:53:04 -0300

    feature: add Navigation and Idle action to viewmodel

--- app/src/main/java/tf/samir/borrowtee/modules/main/presentation/presenter/all_things/AllThingsViewModel.kt
+++ app/src/main/java/tf/samir/borrowtee/modules/main/presentation/presenter/all_things/AllThingsViewModel.kt
@@ -1,19 +1,49 @@
 package tf.samir.borrowtee.modules.main.presentation.presenter.all_things
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.map
 import tf.samir.borrowtee.modules.main.domain.repositories.ThingRepository
 import tf.samir.borrowtee.modules.main.utils.toRecyclerItem
+import timber.log.Timber
 
 
 class AllThingsViewModel @ViewModelInject constructor(private val repository: ThingRepository) : ViewModel() {
+    companion object {
+        const val TAG = "AllThingsViewModel"
+        enum class Action {
+            Idle, NavigateToCreateBorrowing
+        }
+    }
 
     val things = liveData {
         repository.allThings.map { thingList -> thingList.map { it.toRecyclerItem() } }
             .collect { emit(it) }
+    }
+
+    private val _event = MutableLiveData(Action.Idle)
+    val event: LiveData<Action>
+        get() = _event
+
+    init {
+        Timber.tag(TAG).i( "$TAG created!")
+    }
+
+    fun onNavigateToCreateBorrowing() {
+        _event.value = Action.NavigateToCreateBorrowing
+    }
+
+    fun onActionComplete() {
+        _event.value = Action.Idle
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Timber.tag(TAG).i("$TAG destroyed!")
     }
 
 }
