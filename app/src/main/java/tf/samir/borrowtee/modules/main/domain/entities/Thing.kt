107251e6ed0df4303f0e6f1ef1cb1a0925ea1af8
samirtf <samircc20092@gmail.com>
2020-11-14 17:05:36 -0300

    refactor: remove unused classes

--- app/src/main/java/tf/samir/borrowtee/modules/main/domain/entities/Thing.kt
+++
@@ -1,9 +0,0 @@
-package tf.samir.borrowtee.modules.main.domain.entities
-
-data class Thing (
-    val id: String,
-    val name: String,
-    val description: String,
-    @ThingState
-    val state: Int
-)
