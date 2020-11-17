d2ad3ee8965d68b3f0b6e7a8f6c1af824e8fe0d3
samirtf <samircc20092@gmail.com>
2020-11-16 21:35:10 -0300

    feature: add an empty page - new borrowing

--- app/src/main/java/tf/samir/borrowtee/modules/main/view/all_things/AllThingsFragment.kt
+++ app/src/main/java/tf/samir/borrowtee/modules/main/view/all_things/AllThingsFragment.kt
@@ -9,6 +9,7 @@ import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import tf.samir.borrowtee.databinding.FragmentAllThingsBinding
 import tf.samir.borrowtee.modules.main.presentation.presenter.all_things.AllThingsViewModel
@@ -61,8 +62,8 @@ class AllThingsFragment : Fragment() {
     }
 
     private fun navigateToCreateBorrowing() {
-        Toast.makeText(requireContext(), "navigating to create borrowing",
-            Toast.LENGTH_SHORT).show()
+        this.findNavController().navigate(AllThingsFragmentDirections
+            .actionNavigationHomeToCreateBorrowingActivity())
     }
 
     override fun onDestroyView() {
