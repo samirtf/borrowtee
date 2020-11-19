7c5f71e251c3384e625772fc7725b02f82ec4104
samirtf <samircc20092@gmail.com>
2020-11-19 00:49:20 -0300

    feature: add android hyper activity

---
+++ core/src/main/java/ts/samir/core/base/HyperActivity.kt
@@ -0,0 +1,41 @@
+package ts.samir.core.base
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import timber.log.Timber
+
+/**
+ * @author Rohit Surwase
+ * @author https://github.com/RohitSurwase
+ */
+abstract class HyperActivity<STATE, EFFECT, EVENT, ViewModel: AndroidHyperViewModel<STATE, EFFECT, EVENT>> : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "HyperActivity"
+    }
+
+    abstract val viewModel: ViewModel
+
+    private val viewStateObsever = Observer<STATE> {
+        Timber.tag(TAG).d("observed viewState : $it")
+        renderViewState(it)
+    }
+
+    abstract fun renderViewState(viewState: STATE)
+
+    private val viewEffectObserver = Observer<EFFECT> {
+        Timber.tag(TAG).d("observed viewEffect : $it")
+        renderViewEffect(it)
+    }
+
+    abstract fun renderViewEffect(viewEffect: EFFECT)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Registering observers
+        viewModel.viewStates.observe(this, viewStateObsever)
+        viewModel.viewEffects.observe(this, viewEffectObserver)
+    }
+
+}
