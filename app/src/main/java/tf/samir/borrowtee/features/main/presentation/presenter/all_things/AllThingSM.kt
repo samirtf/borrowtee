ab89e987e715390806ff282d05177111c795b633
samirtf <samircc20092@gmail.com>
2020-11-21 01:49:34 -0300

    enhancement: add All Things SM

---
+++ app/src/main/java/tf/samir/borrowtee/features/main/presentation/presenter/all_things/AllThingSM.kt
@@ -0,0 +1,20 @@
+package tf.samir.borrowtee.features.main.presentation.presenter.all_things
+
+import tf.samir.domain.entities.ThingEntity
+
+data class AllThingViewState(val fetchStatus: FetchStatus, val allThings: List<ThingEntity>)
+
+sealed class AllThingsEffect {
+    data class ShowToast(val message: String) : AllThingsEffect()
+}
+
+sealed class AllThingsEvent {
+    object FabClicked : AllThingsEvent()
+    object FetchAllThings : AllThingsEvent()
+}
+
+sealed class FetchStatus {
+    object Fetching : FetchStatus()
+    object Fetched : FetchStatus()
+    object NotFetched : FetchStatus()
+}
