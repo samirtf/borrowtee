8b81e0d5011fb8c9fd4ceb0a4a16d645e768cadc
Samir Trajano Feitosa <samircc20092@gmail.com>
2020-12-16 02:58:24 -0300

    feature: add recyclerview swipe buttons

--- app/src/main/java/tf/samir/borrowtee/features/main/view/all_things/AllThingsFragment.kt
+++ app/src/main/java/tf/samir/borrowtee/features/main/view/all_things/AllThingsFragment.kt
@@ -1,17 +1,20 @@
 package tf.samir.borrowtee.features.main.view.all_things
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.RecyclerView
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_all_things.*
+import tf.samir.borrowtee.R
 import tf.samir.borrowtee.databinding.FragmentAllThingsBinding
 import tf.samir.borrowtee.features.main.presentation.presenter.all_things.*
+import tf.samir.borrowtee.features.main.utils.*
 import tf.samir.core.base.HyperFragment
 import timber.log.Timber
 
@@ -49,6 +52,71 @@ class AllThingsFragment :
                 it.lifecycleOwner = viewLifecycleOwner
                 it.fab.setOnClickListener {
                     viewModel.handle(AllThingsViewEvent.FabClicked)
+                }
+                object : ItemSwipeHelper(requireContext(), it.recyclerView, 200) {
+                    override fun instantiateItemButton(
+                        viewHolder: RecyclerView.ViewHolder,
+                        buffer: MutableList<ButtonItem>
+                    ) {
+
+                        buffer.add(
+                            ButtonItemImpl(
+                                requireContext(),
+                                "Edit",
+                                30,
+                                R.drawable.ic_baseline_create_24,
+                                Color.parseColor("#007FFF"),
+                                object : ItemClickListener {
+                                    override fun onClick(position: Int) {
+                                        Toast.makeText(
+                                            requireContext(),
+                                            "EDIT ID$position",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                    }
+
+                                }
+                            )
+                        )
+                        buffer.add(
+                            ButtonItemImpl(
+                                requireContext(),
+                                "Load",
+                                30,
+                                R.drawable.ic_baseline_create_24,
+                                Color.parseColor("#ff7400"),
+                                object : ItemClickListener {
+                                    override fun onClick(position: Int) {
+                                        Toast.makeText(
+                                            requireContext(),
+                                            "EDIT ID$position",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                    }
+
+                                }
+                            )
+                        )
+                        buffer.add(
+                            TextButtonItem(
+                                "Delete",
+                                30,
+                                Color.parseColor("#ff0000"),
+                                object : ItemClickListener {
+                                    override fun onClick(position: Int) {
+                                        Toast.makeText(
+                                            requireContext(),
+                                            "DELETE ID$position",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                    }
+
+                                }
+                            )
+                        )
+                    }
+
+
                 }
             }
     }
