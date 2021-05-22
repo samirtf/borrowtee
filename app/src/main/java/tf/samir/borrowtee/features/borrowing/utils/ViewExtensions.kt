e416d1f137886a4660e525283ba2d61b450dc4cf
Samir Trajano Feitosa <samircc20092@gmail.com>
2021-05-22 15:46:12 -0300

    wip: add getPermissionToReadExternalStorage method

--- app/src/main/java/tf/samir/borrowtee/features/borrowing/utils/ViewExtensions.kt
+++ app/src/main/java/tf/samir/borrowtee/features/borrowing/utils/ViewExtensions.kt
@@ -1,9 +1,35 @@
 package tf.samir.borrowtee.features.borrowing.utils
 
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 
 @BindingAdapter("setLoading")
 fun setLoading(view: View, value: Boolean) {
     view.visibility = if (value) View.VISIBLE else View.GONE
+}
+
+
+const val READ_EXTERNAL_PERMISSIONS_REQUEST = 1
+fun Activity.getPermissionToReadExternalStorage() {
+    if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+        != PackageManager.PERMISSION_GRANTED) {
+
+        if (shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            // The permission is NOT already granted.
+            // Check if the user has been asked about this permission already and denied
+            // it. If so, we want to give more explanation about why the permission is needed.
+            if (shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                // Show our own UI to explain the user why we need to read the external storage
+                // before actually requesting the permission and showing the default UI
+            }
+
+            // Fire off an async request to actually get the permission
+            // This will show the standard permission request dialog UI
+            requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), READ_EXTERNAL_PERMISSIONS_REQUEST)
+        }
+    }
 }
